0305d41f779434991111602b3fc4b736cbd055d1
James Cameron <51986409+Llama3013@users.noreply.github.com>
2021-11-06 14:12:13 +1100

    Added pause server command

--- src/SinglePauseMod.cs
+++ src/SinglePauseMod.cs
@@ -2,8 +2,9 @@
 using Vintagestory.API.Common;
 using Vintagestory.Client.NoObf;
 using Vintagestory.GameContent;
-using Vintagestory.API.Config;
 using System.Reflection;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
 
 [assembly: ModInfo("SinglePause",
     Description = "A quick mod made to pause the game when you open handbook on single player.",
@@ -36,6 +37,39 @@ namespace SinglePause
             harmony.PatchAll(Assembly.GetExecutingAssembly());
 
         }
+
+        ICoreServerAPI api;
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            base.StartServerSide(api);
+            this.api = api;
+
+            api.RegisterCommand("pause", "Pauses the game", "/pause", PauseCommand);
+            //api.RegisterCommand("resume", "Resumes the game", "/resume", ResumeCommand);
+        }
+
+        public void PauseCommand(IServerPlayer player, int groupId, CmdArgs args)
+        {
+            player.SendMessage(GlobalConstants.GeneralChatGroup, "Your game is paused", EnumChatType.Notification);
+            if (API.api != null && !API.capi.IsGamePaused && API.capi.IsSinglePlayer)
+            {
+                Vintagestory.Client.NoObf.ClientMain clientMain = (Vintagestory.Client.NoObf.ClientMain)API.api.World;
+                clientMain.PauseGame(true);
+                //API.api.Logger.Debug("should pause");
+            }
+        }
+
+        //commands don't work when the game is paused
+        /*public void ResumeCommand(IServerPlayer player, int groupId, CmdArgs args)
+        {
+            player.SendMessage(GlobalConstants.GeneralChatGroup, "Your game is resumed", EnumChatType.Notification);
+            if (API.api != null && API.capi.IsGamePaused && API.capi.IsSinglePlayer)
+            {
+                Vintagestory.Client.NoObf.ClientMain clientMain = (Vintagestory.Client.NoObf.ClientMain)API.api.World;
+                clientMain.PauseGame(false);
+                //API.capi.Logger.Debug("should unpause");
+            }
+        }*/
     }
 
 
@@ -66,6 +100,35 @@ namespace SinglePause
             }
         }
     }
+
+    //This causes the game to crash on startup
+    /*[HarmonyPatch(typeof(GuiDialogCreateCharacter), "OnGuiOpened")]
+    public class Patch_GuiDialogCreateCharacter_OnGuiOpened
+    {
+        static void Postfix()
+        {
+            if (API.api != null && !API.capi.IsGamePaused && API.capi.IsSinglePlayer)
+            {
+                Vintagestory.Client.NoObf.ClientMain clientMain = (Vintagestory.Client.NoObf.ClientMain)API.api.World;
+                clientMain.PauseGame(true);
+                //API.api.Logger.Debug("should pause");
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(GuiDialogCreateCharacter), "OnGuiClosed")]
+    public class Patch_GuiDialogCreateCharacter_OnGuiClosed
+    {
+        static void Postfix()
+        {
+            if (API.api != null && API.capi.IsGamePaused && API.capi.IsSinglePlayer)
+            {
+                Vintagestory.Client.NoObf.ClientMain clientMain = (Vintagestory.Client.NoObf.ClientMain)API.api.World;
+                clientMain.PauseGame(false);
+                //API.capi.Logger.Debug("should unpause");
+            }
+        }
+    }*/
 
     /*[HarmonyPatch(typeof(GuiDialogBlockEntity), "OnGuiOpened")]
     public class Patch_GuiDialogBlockEntity_OnGuiOpened
